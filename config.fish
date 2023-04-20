fb7da220356aa972cef455e72a2720202142f268
G Roques <groques360@gmail.com>
2023-04-20 15:57:02 -0500

    Add 'gd' and 'gcia' abbreviations

--- config.fish
+++ config.fish
@@ -12,6 +12,8 @@ abbr -a -- g git
 abbr -a -- gco 'git checkout'
 abbr -a -- ga 'git add'
 abbr -a -- gs 'git status'
+abbr -a -- gd 'git diff'
 abbr -a --set-cursor='%' -- gci 'git commit -m "%"'
+abbr -a --set-cursor='%' -- gcia 'git commit -am "%"'
 
 # -------------------------------------------------------------------

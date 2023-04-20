19eb07ec1534484b2eb27485e2b2501993b76483
G Roques <groques360@gmail.com>
2023-04-20 18:00:21 -0500

    Move comment closer to nerd font char

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,8 +2,8 @@ function fish_prompt
     set_color $fish_color_cwd
     echo -n (prompt_pwd --full-length-dirs 3)
     set_color yellow
-    # nf-fa-chevron_right
     printf '%s' (fish_git_prompt)
     set_color normal
+    # nf-fa-chevron_right
     printf '  '
 end

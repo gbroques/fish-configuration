6c2c49cef08d53a87c2eff84066bfb8c5c3c9db7
G Roques <groques360@gmail.com>
2023-04-20 16:12:20 -0500

    Add 'gp' abbreviation for 'git push'

--- config.fish
+++ config.fish
@@ -13,6 +13,7 @@ abbr -a -- gco 'git checkout'
 abbr -a -- ga 'git add'
 abbr -a -- gs 'git status'
 abbr -a -- gd 'git diff'
+abbr -a -- gp 'git push'
 abbr -a --set-cursor='%' -- gci 'git commit -m "%"'
 abbr -a --set-cursor='%' -- gcia 'git commit -am "%"'
 

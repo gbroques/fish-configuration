062a4ac937944e44bdde2f772d6c522893ae87e6
G Roques <groques360@gmail.com>
2023-04-20 16:25:57 -0500

    Add (p)u(s)h and (p)u(l)l abbreviations
    Alphabetize git abbreviations

--- config.fish
+++ config.fish
@@ -9,12 +9,13 @@ abbr -a -- vim nvim
 
 # Git
 abbr -a -- g git
-abbr -a -- gco 'git checkout'
 abbr -a -- ga 'git add'
-abbr -a -- gs 'git status'
-abbr -a -- gd 'git diff'
-abbr -a -- gp 'git push'
 abbr -a --set-cursor='%' -- gcm 'git commit -m "%"'
 abbr -a --set-cursor='%' -- gcma 'git commit -am "%"'
+abbr -a -- gco 'git checkout'
+abbr -a -- gs 'git status'
+abbr -a -- gd 'git diff'
+abbr -a -- gps 'git push'
+abbr -a -- gpl 'git pull'
 
 # -------------------------------------------------------------------

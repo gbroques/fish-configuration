31264f2ea8a81c16ef6230a352c21203fc6c604e
G Roques <groques360@gmail.com>
2023-11-17 19:01:20 -0600

    Alphabetize git abbreviations

--- config.fish
+++ config.fish
@@ -15,10 +15,10 @@ abbr -a -- gb 'git branch'
 abbr -a --set-cursor='%' -- gcm 'git commit -m "%"'
 abbr -a --set-cursor='%' -- gcma 'git commit -am "%"'
 abbr -a -- gco 'git checkout'
-abbr -a -- gs 'git status'
 abbr -a -- gd 'git diff'
-abbr -a -- gps 'git push'
 abbr -a -- gpl 'git pull'
+abbr -a -- gps 'git push'
+abbr -a -- gs 'git status'
 
 # Miscellaneous
 abbr -a -- fcd 'cd (fd --type d | fzf)' # fuzzy cd

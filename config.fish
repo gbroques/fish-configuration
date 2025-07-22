30dd223049681f8c8d4be24cebe98743188cc08d
G Roques <groques360@gmail.com>
2025-07-22 15:33:14 -0500

    Add /usr/local/bin to fish_user_paths
    This contains code executable in macOS

--- config.fish
+++ config.fish
@@ -68,6 +68,7 @@ end
 set -l os (uname)
 if test "$os" = Darwin
   set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+  set -U fish_user_paths /usr/local/bin $fish_user_paths
 end
 
 if string match -q "*CYGWIN*" (uname -a)
